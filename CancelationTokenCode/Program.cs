7ac9ea39adb3f080b92d1dcac38a1bd30072b0a8
Nikita <nikita.poznyak@zyfra.com>
2024-02-07 14:57:52 +0300

    Добавьте файлы проекта.

---
+++ CancelationTokenCode/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace CancelationTokenCode
+{
+    public class Program
+    {
+        static async Task Main()
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+            CancellationToken token = cts.Token;
+
+            Task task = Task.Run(() => DoWorkAsync(token), token);
+
+            await Task.Delay(1000);
+
+            cts.Cancel();
+
+            await task;
+
+            Console.WriteLine($"Task Status: {task.Status}");
+            cts.Dispose();
+        }
+
+        static async Task DoWorkAsync(CancellationToken token)
+        {
+            try
+            {
+                for (int i = 1; i < 10; i++)
+                {
+                    token.ThrowIfCancellationRequested();
+
+                    Console.WriteLine($"Квадрат числа {i} равен {i * i}");
+
+                    await Task.Delay(200);
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine("Операция прервана");
+            }
+        }
+    }
+}
